0b88bfb9a6bbc16cd7fab05e56bcf49f4b1b034d
Runner <1598383487@qq.com>
2024-09-27 16:19:45 +0800

    add cam.py

---
+++ model/cam.py
@@ -0,0 +1,138 @@
+"""
+unet
+"""
+import os
+import torch
+from unet import UNet
+import torch.nn.functional as F
+from PIL import Image
+import numpy as np
+from matplotlib import pyplot as plt
+# cam
+from pytorch_grad_cam import GradCAM,ScoreCAM,GradCAMPlusPlus,AblationCAM,XGradCAM,EigenCAM,FullGrad
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image, preprocess_image
+
+
+class SegmentationModelOutputWrapper(torch.nn.Module):
+    def __init__(self, model): 
+        super(SegmentationModelOutputWrapper, self).__init__()
+        self.model = model
+        
+    def forward(self, x):
+        return self.model(x)["out"]
+    
+class SemanticSegmentationTarget:
+    def __init__(self, category, mask):
+        self.category = category
+        self.mask = torch.from_numpy(mask)
+        if torch.cuda.is_available():
+            self.mask = self.mask.cuda()
+        
+    def __call__(self, model_output):
+        return (model_output[self.category, :, : ] * self.mask).sum()
+
+# 加载图像
+img_path = "/mnt/c/VScode/WS-Hub/WS-U2net/Image1 - 003.jpeg"
+img = Image.open(img_path).resize((224,224)).convert('RGB')
+
+# 预处理图像
+rgb_img = np.float32(img) / 255
+
+# 将图片转为tensor
+input_tensor = preprocess_image(rgb_img,
+                                mean=[0.485, 0.456, 0.406],
+                                std=[0.229, 0.224, 0.225])
+
+# 加载模型权重
+weights_path = '/mnt/c/VScode/WS-Hub/WS-U2net/U-2-Net/results/save_weights/unet/L: FocalLoss--S: CosineAnnealingLR/lr: 0.001-wd: 0.0001/2024-09-27_09:31:58/model_best.pth'
+checkpoint = torch.load(weights_path)
+state_dict = checkpoint['model']
+
+# 创建模型实例
+model = UNet(in_channels=3, n_classes=4, p=0.25)
+
+# 加载模型权重
+model.load_state_dict(state_dict)
+
+# 确保在推理模式下运行模型
+model.eval()
+if torch.cuda.is_available():
+    model = model.cuda()
+    input_tensor = input_tensor.cuda()
+    
+output = model(input_tensor)
+
+# 模型输出为字典
+# model = SegmentationModelOutputWrapper(model)
+# output = model(input_tensor)
+normalized_masks = torch.nn.functional.softmax(output, dim=1).cpu()
+
+# 分割类别
+sem_classes = [
+    '__background__', 'Organic matter', 'Organic pore', 'Inorganic pore'
+]
+
+# 获取类索引
+sem_class_to_idx = {cls: idx for (idx, cls) in enumerate(sem_classes)}
+OM_category = sem_class_to_idx["Organic matter"]
+OP_category = sem_class_to_idx["Organic pore"]
+IP_category = sem_class_to_idx["Inorganic pore"]
+
+# OM
+OM_mask = normalized_masks[0, :, :, :].argmax(axis=0).detach().cpu().numpy()
+OM_mask_uint8 = 255 * np.uint8(OM_mask == OM_category)
+OM_mask_float = np.float32(OM_mask == OM_category)
+
+# OP
+OP_mask = normalized_masks[0, :, :, :].argmax(axis=0).detach().cpu().numpy()
+OP_mask_uint8 = 255 * np.uint8(OP_mask == OP_category)
+OP_mask_float = np.float32(OP_mask == OP_category)
+
+# IP
+IP_mask = normalized_masks[0, :, :, :].argmax(axis=0).detach().cpu().numpy()
+IP_mask_uint8 = 255 * np.uint8(IP_mask == IP_category)
+IP_mask_float = np.float32(IP_mask == IP_category)
+
+# 指定目标层
+target_layers = [model.up4.conv]
+
+# OM
+OM_targets = [SemanticSegmentationTarget(OM_category, OM_mask_float)]
+with GradCAM(model=model,
+             target_layers=target_layers,
+             ) as cam:
+    grayscale_cam = cam(input_tensor=input_tensor,
+                        targets=OM_targets)[0, :]
+    OM_cam_image = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+
+# OP
+OP_targets = [SemanticSegmentationTarget(OP_category, OP_mask_float)]
+with GradCAM(model=model,
+             target_layers=target_layers,
+             ) as cam:
+    grayscale_cam = cam(input_tensor=input_tensor,
+                        targets=OP_targets)[0, :]
+    OP_cam_image = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+
+# IP
+IP_targets = [SemanticSegmentationTarget(IP_category, IP_mask_float)]
+with GradCAM(model=model,
+             target_layers=target_layers,
+             ) as cam:
+    grayscale_cam = cam(input_tensor=input_tensor,
+                        targets=IP_targets)[0, :]
+    IP_cam_image = show_cam_on_image(rgb_img, grayscale_cam, use_rgb=True)
+    
+# 保存图片
+save_path = "/mnt/c/VScode/WS-Hub/WS-U2net/U-2-Net/cam_img/"
+OM_cam_img = Image.fromarray(OM_cam_image)
+OM_cam_img.save(os.path.join(save_path, "OM_cam_img.jpg"))
+
+OP_cam_img = Image.fromarray(OP_cam_image)
+OP_cam_img.save(os.path.join(save_path, "OP_cam_img.jpg"))
+
+IP_cam_img = Image.fromarray(IP_cam_image)
+IP_cam_img.save(os.path.join(save_path, "IP_cam_img.jpg"))
+
+print("映射完成！！")
